3d9b1c7900471a167539d5cb9bfd07f360006e6e
gceballos23 <103612856+gceballos23@users.noreply.github.com>
2022-06-06 15:42:08 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+
+
+function aleatorioBoolean(): boolean{
+  
+}
+
+function cargarButacas(cine : array){
+
+}
+
+function mostrarOcupacion(cine : array){
+  
+}
+
+
+let numero: number = Number(data.value);
+switch (numero) { 
+  case 0:
+
+    console.log("El nro ingresado es cero");
+    break;
+  default:  
+    if (numero % 2 > 0) {
+      console.log("Numero Impar");
+    } else {
+      console.log("Numero Par");
+    
+    }
+ }
